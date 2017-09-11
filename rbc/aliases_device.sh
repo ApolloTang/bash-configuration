082065688497fb8aa97d3c0a9bc1dfd551631dcb
Apollo Tang <tang.apollo@gmail.com>
2017-09-11 12:04:04 -0400

    added work dir rbc

---
+++ rbc/aliases_device.sh
@@ -0,0 +1,20 @@
+# Debug Messsages -----------------------------------------------
+echo 'echo from ~/1/1-sy/configurations/bash/lights/aliases_device.sh'
+
+################################################
+#
+#/ Change Directories ------------------------
+
+
+#/ Application --------------------------------
+
+#alias chromium='/Applications/Chromium.app/Contents/MacOS/Chromium &'
+# alias chrome='/usr/bin/open -a "/Applications/Google Chrome.app"'
+# alias chrome1="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory=\"Default\""
+# alias chrome2="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory=\"Profile 1\""
+
+#/ Note ----------------------------------------
+
+
+################################################
+#echo '.bash_aliases_device_lights reading done'
---
+++ rbc/bashrc_device.sh
@@ -0,0 +1,50 @@
+# Debug Messsages -----------------------------------------------
+echo 'echo from ~/1/1-sy/configurations/bash/lights/bashrc_device.sh'
+
+export IAM=rbc
+export PLATFORM=mac
+export XDG_CONFIG_HOME="/Users/teckchoontang/.config"
+
+
+## Path
+# @NOTE: reverse order: the last one will be prepended to the path
+#
+PATH="$HOME/Applications/kdiff3.app/Contents/MacOS":$PATH   # kdiff3
+# PATH=/Users/apollotang/opt/local/anaconda2/bin:$PATH
+#
+# @NOTE: normal order: the last one will be appended to the path
+#
+# PATH=$PATH:"$HOME/opt/devtools/autotools-bin/bin"
+export PATH
+
+
+##
+# Ruby
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+# ##
+# # Android
+# export ANDROID_HOME=${HOME}/Library/Android/sdk
+# export PATH=${PATH}:${ANDROID_HOME}/emulator  # this must be before tools, else wrong emulator will be evoke
+# export PATH=${PATH}:${ANDROID_HOME}/tools
+# export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+
+
+# ##
+# # for nvm
+# export NVM_DIR="/Users/apollotang/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+
+# ##
+# # Scala
+# export SCALA_HOME="$HOME/opt/local/scala-2.11.2/bin"
+
+
+##/ Project directory
+
+##/ Run things
+# archey
+
+###################################
+#echo '.bashrc_lights reading done'
