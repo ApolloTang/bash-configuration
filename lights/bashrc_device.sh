388a86a975b0137af0fdb00018e0d79f86910e1b
Apollo Tang <tang.apollo@gmail.com>
2018-06-19 11:51:32 -0400

    add path to bin 1-sy bin folder

--- lights/bashrc_device.sh
+++ lights/bashrc_device.sh
@@ -15,6 +15,7 @@ shopt -s globstar
 ## Path
 # @NOTE: reverse order: the last one will be prepended to the path
 #
+PATH="$HOME/1/1-sy/scripts/bin":$PATH   # custom scripts link to here
 PATH="$HOME/Applications/kdiff3.app/Contents/MacOS":$PATH   # kdiff3
 PATH=/Users/apollotang/opt/local/anaconda2/bin:$PATH
 #
