ec8bdf48ce379b5eb63dca11f93f4fd7102455ff
Apollo Tang <tang.apollo@gmail.com>
2018-10-13 14:51:22 -0400

    refactoring bashrc structure

--- common/bashrc_common.sh
+++ common/bashrc_common.sh
@@ -1,25 +1,10 @@
-# # Debug Messsages -----------------------------------------------
+## Debug Messsages -----------------------------------------------
 # echo 'echo from ~/1/1-sy/configurations/bash/common/bashrc_common.sh'
-
-##
-# Path
-# @NOTE: reverse order: the last one will be prepended to the path
-#
-PATH="$HOME/script/bin2":$PATH
-PATH="$HOME/script/bin":$PATH
-PATH="$HOME/bin":$PATH
-PATH="$dir_sy1/bin":$PATH
-#
-# @NOTE: normal order: the last one will be appended to the path
-#
-# PATH=$PATH:"$HOME/opt/local/adt-bundle-mac-x86_64-20140702/sdk/platform-tools"
-export PATH
-
 
 ##
 # Misc
 export editor_main=mvim
-export DT=$HOME/Desktop/
+export DT=$HOME/Desktop
 export HISTCONTROL=ignoredups
 shopt -s checkwinsize   # After each command, checks the windows size and changes lines and columns
 set -o vi               # Use Vim mode
@@ -27,14 +12,13 @@ export LESS=-RFX        # color for Less
 
 
 ##
-# # START - git completion
-# installation using mac port, see:
-# https://github.com/bobthecow/git-flow-completion/wiki/Install-Bash-git-completion
+#/ START - git completion
+#/ via homebrew, see:
+#/ https://github.com/bobthecow/git-flow-completion/wiki/Install-Bash-git-completion
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
 fi
-# # END  - git completion
-
+#/ END  - git completion
 
 # using the above so coment this one out
 # ##
@@ -44,18 +28,16 @@ fi
 # fi
 # # END  - git completion
 
-
 ##
-# bash completion settings (actually, these are readline settings)
+#/ bash completion settings (actually, these are readline settings)
 bind "set completion-ignore-case on" # note: bind used instead of sticking these in .inputrc
-bind "set bell-style none" # no bell
-bind "set show-all-if-ambiguous On" # show list automatically, without double tab
-
+bind "set bell-style none"           # no bell
+bind "set show-all-if-ambiguous On"  # show list automatically, without double tab
 
 ##
-# Environment for git-diffc
+#/ Environment for git-diffc
 export TMPDIR=~/tmp    # location of temp folder
-export CLEANUP=0                     # don't clean up
-#      [note] DO NOT SET GIT_EXTERNAL_DIFF. IF YOU DO git-diffc WILL NOT WORK
+export CLEANUP=0       # don't clean up
+#/ [note] DO NOT SET GIT_EXTERNAL_DIFF. IF YOU DO git-diffc WILL NOT WORK
 
 
--- lights/bashrc_device.sh
+++ lights/bashrc_device.sh
@@ -7,62 +7,69 @@ export XDG_CONFIG_HOME="/Users/apollotang/.config"
 export RIPGREP_CONFIG_PATH="/Users/apollotang/1/1-sy/configurations/ripgrep/dot.ripgreprc"
 
 
-## Enable Glob support
-# https://gist.github.com/reggi/475793ea1846affbcfe8
+### Enable Glob support
+#/ https://gist.github.com/reggi/475793ea1846affbcfe8
 shopt -s globstar
 
 
-## Path
-# @NOTE: reverse order: the last one will be prepended to the path
-#
+### Path
+#/
+#/ @NOTE: reverse order: the last one will be prepended to the path
+#/
+PATH="$HOME/script/bin2":$PATH
+PATH="$HOME/script/bin":$PATH
+PATH="$HOME/bin":$PATH
 PATH="$HOME/1/1-sy/scripts/bin":$PATH                       # custom scripts link to here
 PATH="$HOME/Applications/kdiff3.app/Contents/MacOS":$PATH   # kdiff3
 # PATH=/Users/apollotang/opt/local/anaconda2/bin:$PATH
-PATH=/Users/apollotang/opt/local/anaconda3/bin:$PATH
+# PATH=/Users/apollotang/opt/local/anaconda3/bin:$PATH
 PATH=/usr/local/Cellar/coreutils/8.30/bin:$PATH             # manually link coreutil b/c gsync has already symlinc to /Library/Application\ Support/GoodSync/gsync
-#
-# @NOTE: normal order: the last one will be appended to the path
-#
+#/
+#/ @NOTE: normal order: the last one will be appended to the path
+#/
 PATH=$PATH:"$HOME/opt/local/scala-2.11.2/bin"
 PATH=$PATH:"/usr/local/Cellar/neovim-dot-app/HEAD/bin/gnvim"
 PATH=$PATH:"$HOME/opt/devtools/autotools-bin/bin"
 export PATH
 
 
-##
-# Ruby
+###
+#/ Ruby
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
-##
-# Android
+
+###
+#/ Android
 export ANDROID_HOME=${HOME}/Library/Android/sdk
 export PATH=${PATH}:${ANDROID_HOME}/emulator  # this must be before tools, else wrong emulator will be evoke
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 
 
-##
-# for nvm
+###
+#/ for nvm
 export NVM_DIR="/Users/apollotang/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 
-##
-# Scala
+###
+#/ Scala
 export SCALA_HOME="$HOME/opt/local/scala-2.11.2/bin"
 
-##
-# local directory
-export dir_server_lib="/Users/apollotang/0/z-local/vendor-lib/"
+
+###
+#/ local directory
+export dir_vendor_server="/Users/apollotang/0/z-local/vendor-server/"
 
 
-##
+###
 #/ Import configuration that change frequently or private
 if [ -f "$dir_config_device/git-sub-local/local-only.sh" ]; then
   . "$dir_config_device/git-sub-local/local-only.sh"
 fi
 
-##/ Run things
+
+###/ Run things
 # archey
 
 ###################################
