b3716b1f5f7c1df99b20203f372148e9a14eb9af
Apollo Tang <tang.apollo@gmail.com>
2018-10-17 23:51:29 -0400

    added alias to run vendor server

--- lights/aliases_device.sh
+++ lights/aliases_device.sh
@@ -14,6 +14,7 @@ alias chrome='/usr/bin/open -a "/Applications/Google Chrome.app"'
 alias chrome1="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory=\"Default\""
 alias chrome2="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory=\"Profile 1\""
 
+alias run-vendor-server="cd ~/0/z-local/vendor-server/; ./start.sh"
 
 ################################################
 #echo '.bash_aliases_device_lights reading done'
